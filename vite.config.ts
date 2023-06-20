cffdb464a3bc52f645a6532799e0e2105dcf8f0e
jasl <jasl9187@hotmail.com>
2023-06-21 04:50:36 +0800

    Upgrade most of dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-// import legacy from '@vitejs/plugin-legacy'
 import Analyze from 'rollup-plugin-visualizer'
 import Unocss from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -8,7 +7,7 @@ import IconsResolver from 'unplugin-icons/resolver'
 import Icons from 'unplugin-icons/vite'
 import { defineConfig, loadEnv } from 'vite'
 import Checker from 'vite-plugin-checker'
-import EslintPlugin from 'vite-plugin-eslint'
+import eslint from 'vite-plugin-eslint2'
 import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 import Pages from 'vite-plugin-pages'
 
@@ -49,7 +48,7 @@ export default defineConfig(({ mode }) => {
           }),
         ],
       }),
-      EslintPlugin(),
+      eslint(),
       Analyze(),
       nodePolyfills(),
       // legacy({
@@ -62,7 +61,7 @@ export default defineConfig(({ mode }) => {
           manualChunks: {
             'react-vendor': ['react', 'react-router-dom', 'react-dom'],
             'wagmi-vendor': ['wagmi', 'viem'],
-            'ui-vendor': ['antd'],
+            'polkadot-js-vendor': ['@polkadot/keyring', '@polkadot/util', '@polkadot/util-crypto'],
           },
         },
       },
