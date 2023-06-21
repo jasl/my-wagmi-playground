430b2f9fcb9945fc3eb4ab29160ff1405ca88be3
jasl <jasl9187@hotmail.com>
2023-06-21 17:58:41 +0800

    wip

--- src/pages/debug/index.tsx
+++ src/pages/debug/index.tsx
@@ -1,6 +1,8 @@
 import { recoverPublicKey } from 'viem'
 import { useAccount, useBalance, useSignMessage, useWalletClient } from 'wagmi'
 
+import { Button } from '@/components/ui/button'
+
 const Debug = () => {
   const evmAddress = useAccount().address
   const evmBalance = useBalance({
@@ -12,9 +14,21 @@ const Debug = () => {
   // const { data: signedMessage } = useSignMessage({ message: rawMessage })
   // const evmPublicKey = recoverPublicKey({ hash: rawMessage, signature: signedMessage })
 
+  const rawMessage = '0x00'
+  const { data: signedMessage, signMessage } = useSignMessage({ message: rawMessage })
+
+  const [publicKey, setPublicKey] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    if (signedMessage) {
+      recoverPublicKey({ hash: rawMessage, signature: signedMessage }).then(setPublicKey)
+    }
+  }, [rawMessage, signedMessage])
+
   return (
     <div className="relative max-w-6xl min-h-[calc(100vh-8rem)] m-auto pt-16 justify-start">
       <div className="py-4 text-lg text-gray font-bold border-b">Debug</div>
+      <Button onClick={(_e) => signMessage()}>Foo</Button>
       <div className="mt-6 border-t border-gray-100">
         <dl className="divide-y divide-gray-100">
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -29,11 +43,11 @@ const Debug = () => {
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">EVM signed message</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"></dd>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{signedMessage}</dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">EVM public key</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"></dd>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{publicKey}</dd>
           </div>
         </dl>
       </div>
